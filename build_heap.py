bbc3301d0c937fb5f86ddb20b7211801ab42067f
221RDB452 <124876380+221RDB452@users.noreply.github.com>
2023-03-18 11:59:36 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,31 +1,29 @@
 def sift_down(data, i, swaps):
-    n = len(data)
     min_index = i
-    l = 2 * i + 1
-    if l < n and data[l] < data[min_index]:
-        min_index = l
-    r = 2 * i + 2
-    if r < n and data[r] < data[min_index]:
-        min_index = r
+    left_child = 2 * i + 1
+    if left_child < len(data) and data[left_child] < data[min_index]:
+        min_index = left_child
+    right_child = 2 * i + 2
+    if right_child < len(data) and data[right_child] < data[min_index]:
+        min_index = right_child
     if i != min_index:
-        swaps.append((i, min_index))
         data[i], data[min_index] = data[min_index], data[i]
+        swaps.append((i, min_index))
         sift_down(data, min_index, swaps)
-
 def build_heap(data):
-    n = len(data)
     swaps = []
-    for i in range(n // 2, -1, -1):
+    for i in range(len(data) // 2, -1, -1):
         sift_down(data, i, swaps)
     return swaps
-
 def main():
     n = int(input())
     data = list(map(int, input().split()))
+    assert len(data) == n
     swaps = build_heap(data)
     print(len(swaps))
-    for swap in swaps:
-        print(swap[0], swap[1])
+    for i, j in swaps:
+        print(i, j)
 
 if __name__ == "__main__":
     main()
+
