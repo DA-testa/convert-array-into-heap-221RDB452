9937894d0e7bb516cde9b78727f1df64db4b9e9f
221RDB452 <124876380+221RDB452@users.noreply.github.com>
2023-03-18 12:32:08 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,10 +1,10 @@
 
 def sift_down(data, i, swaps):
     min_index = i
-    left_child = 2 * i + 1
+    left_child = 2*i+1
     if left_child < len(data) and data[left_child] < data[min_index]:
         min_index = left_child
-    right_child = 2 * i + 2
+    right_child = 2*i+2
     if right_child < len(data) and data[right_child] < data[min_index]:
         min_index = right_child
     if i != min_index:
@@ -13,12 +13,12 @@ def sift_down(data, i, swaps):
         sift_down(data, min_index, swaps)
 def build_heap(data):
     swaps = []
-    for i in range(len(data) // 2, -1, -1):
-        sift_down(data, i, swaps)
+    for i in range(len(data) // 2,-1,-1):
+        sift_down(data,i, swaps)
     return swaps
 def main():
     try:
-        n = int(input())
+        n =int(input())
         data = list(map(int, input().split()))
         assert len(data) == n
         for x in data:
@@ -30,12 +30,7 @@ def main():
     swaps = build_heap(data)
     print(len(swaps))
     for i, j in swaps:
-        print(i, j)
-
-
-
-
-        
+        print(i,j)
 if __name__ == "__main__":
     main()
 
